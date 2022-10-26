9322f3f43b5651d7178fe21adaa8a12c4fb49a45
waldnerique <waldenrique@gmail.com>
2022-10-26 01:00:30 +0100

    6

--- carros/admin.py
+++ carros/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Carros
 
 class ListaCarro(admin.ModelAdmin):
-    list_display = ('id', 'carro')
-    list_display_links = ('id', 'carro', 'modelo')
-    search_fields = ('carro', 'modelo')
+    list_display = ('id', 'carro', 'marca')
+    list_display_links = ('id', 'carro', 'marca')
+    search_fields = ('carro', 'marca')
+    list_filter = ('marca',)
     list_per_page = 10
 
 admin.site.register(Carros, ListaCarro)
--- carros/models.py
+++ carros/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Carros(models.Model):
     carro = models.CharField(max_length=100)
-    modelo = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
 
 
