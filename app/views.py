9225cfa85e14f118a8470785ececa9dda0a861e6
waldnerique <waldenrique@gmail.com>
2022-10-26 13:38:11 +0100

    deshboard

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render(request,'login.html')
+
+def logout(request):
+    return render(request,'logout.html')
+
+def deshboard(request):
+    return render(request,'deshboard.html')
--- oficina/urls.py
+++ oficina/urls.py
@@ -10,9 +10,6 @@ urlpatterns = [
     path('quemsomos', views.quemsomos, name='quemsomos'),
     path('localizacao', views.localizacao, name='localizacao'),
     path('blog', views.blog, name='blog'),
-    path('login', views.blog, name='login'),
-    path('logout', views.blog, name='logout'),
-    path('dashboard', views.blog, name='dashboard'),
-    
+ 
     
 ]
