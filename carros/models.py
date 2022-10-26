05f9f9c8bb5da520ae134e40c6d766a99fe43786
waldnerique <waldenrique@gmail.com>
2022-10-26 01:37:54 +0100

    1 parte ok

--- carros/models.py
+++ carros/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from clientes.models import Clientes
 
 class Carros(models.Model):
+    nome = models.ForeignKey(Clientes, on_delete=models.CASCADE)
     carro = models.CharField(max_length=100)
     marca = models.CharField(max_length=100)
 
--- clientes/models.py
+++ clientes/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Clientes(models.Model):
     nome = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    def __str__(self) :
+        return self.nome
 
 
