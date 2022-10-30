4401497c8316556336ab7931b2da25d5ba2ec44d
waldnerique <waldenrique@gmail.com>
2022-10-30 18:32:18 +0000

    l

--- ordem_servico/admin.py
+++ ordem_servico/admin.py
@@ -4,8 +4,8 @@ from .models import Ordens
 class ListaOrdens(admin.ModelAdmin):
     list_display = ('id', 'nome', 'carro', 'ordem')
     list_display_links = ('id', 'nome', 'carro', 'ordem')
+    search_fields = ('ordem',)
     list_filter = ('ordem',)
-    search_fields = ('carro', 'marca','nome')
     list_per_page = 10
     
 
