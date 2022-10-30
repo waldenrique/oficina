ed4a35c92cb380214134a423612f54d370f710b3
waldnerique <waldenrique@gmail.com>
2022-10-30 18:14:59 +0000

    rrrr

--- clientes/admin.py
+++ clientes/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Clientes
 
 class ListaClientes(admin.ModelAdmin):
-    list_display = ('id', 'nome')
-    list_display_links = ('id', 'nome')
+    list_display = ('id', 'nome', 'email','cpf')
+    list_display_links = ('id', 'nome','email','cpf')
     search_fields = ('nome', 'email')
     list_per_page = 10
 
--- ordem_servico/admin.py
+++ ordem_servico/admin.py
@@ -4,7 +4,9 @@ from .models import Ordens
 class ListaOrdens(admin.ModelAdmin):
     list_display = ('id', 'nome', 'carro', 'ordem')
     list_display_links = ('id', 'nome', 'carro', 'ordem')
-    search_fields = ('nome', 'carro', 'ordem')
+    list_filter = ('ordem',)
+    search_fields = ('carro', 'marca','nome')
     list_per_page = 10
+    
 
 admin.site.register(Ordens, ListaOrdens)
